bb84f10cb9c58564b00f0c4981b477df858a8e0d
Taylor Salo <tsalo006@fiu.edu>
2020-12-18 14:40:25 -0500

    Fix test.

--- mapca/tests/test_mapca.py
+++ mapca/tests/test_mapca.py
@@ -1,13 +1,22 @@
 """
-Tests for maPCA
+Tests for mapca
 """
 
 import numpy as np
 import nibabel as nib
-from tedana import decomposition
 from pytest import raises
-from tedana.decomposition.ma_pca import _autocorr, _check_order, _parzen_win
-from tedana.decomposition.ma_pca import _subsampling, _kurtn, _icatb_svd, _eigensp_adj
+
+from mapca.mapca import ma_pca
+from mapca.utils import (
+    ent_rate_sp,
+    _autocorr,
+    _check_order,
+    _parzen_win,
+    _subsampling,
+    _kurtn,
+    _icatb_svd,
+    _eigensp_adj,
+)
 
 
 def test_autocorr():
@@ -62,7 +71,7 @@ def test_ent_rate_sp():
     Check that ent_rate_sp runs correctly, i.e. returns a float
     """
     test_data = np.random.rand(200, 10, 10)
-    ent_rate = decomposition.ent_rate_sp(test_data, 1)
+    ent_rate = ent_rate_sp(test_data, 1)
     assert isinstance(ent_rate, float)
     assert ent_rate.ndim == 0
     assert ent_rate.size == 1
@@ -70,13 +79,13 @@ def test_ent_rate_sp():
     # Checks ValueError with std = 0
     test_data = np.ones((200, 10, 10))
     with raises(ValueError) as errorinfo:
-        ent_rate = decomposition.ent_rate_sp(test_data, 1)
+        ent_rate = ent_rate_sp(test_data, 1)
     assert "Divide by zero encountered" in str(errorinfo.value)
 
     # Checks ValueError with incorrect matrix dimensions
     test_data = np.ones((200, 10, 10, 200))
     with raises(ValueError) as errorinfo:
-        ent_rate = decomposition.ent_rate_sp(test_data, 1)
+        ent_rate = ent_rate_sp(test_data, 1)
     assert "Incorrect matrix dimensions" in str(errorinfo.value)
 
 
@@ -145,7 +154,7 @@ def test_ma_pca():
     test_mask_img = nib.nifti1.Nifti1Image(test_mask, xform)
 
     # Testing AIC option
-    u, s, varex_norm, v = decomposition.ma_pca(test_img, test_mask_img, "aic")
+    u, s, varex_norm, v = ma_pca(test_img, test_mask_img, "aic")
 
     assert u.shape[0] == n_vox_total
     assert s.shape[0] == 1
@@ -155,7 +164,7 @@ def test_ma_pca():
     del u, s, varex_norm, v
 
     # Testing KIC option
-    u, s, varex_norm, v = decomposition.ma_pca(test_img, test_mask_img, "kic")
+    u, s, varex_norm, v = ma_pca(test_img, test_mask_img, "kic")
 
     assert u.shape[0] == n_vox_total
     assert s.shape[0] == 1
@@ -165,7 +174,7 @@ def test_ma_pca():
     del u, s, varex_norm, v
 
     # Testing MDL option
-    u, s, varex_norm, v = decomposition.ma_pca(test_img, test_mask_img, "mdl")
+    u, s, varex_norm, v = ma_pca(test_img, test_mask_img, "mdl")
 
     assert u.shape[0] == n_vox_total
     assert s.shape[0] == 1
