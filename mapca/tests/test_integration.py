618787395eedb8433a4594e579c2d8cbc5f0c226
Eneko <enekouru@gmail.com>
2021-01-07 10:45:22 +0100

    Start working on integration test

---
+++ mapca/tests/test_integration.py
@@ -0,0 +1,37 @@
+"""
+Integration test for mapca.
+"""
+
+import nibabel as nib
+import numpy as np
+import os.path as op
+
+from mapca.mapca import ma_pca
+
+
+def get_resources_path():
+    """Return the path to test resources.
+
+    Returns the path to test resources, terminated with separator.
+    Resources are kept outside package folder in "resources".
+    Based on function by Yaroslav Halchenko used in Neurosynth Python package.
+
+    Returns
+    -------
+    resource_path : str
+        Absolute path to resources folder.
+    """
+    return op.abspath(op.join(op.dirname(__file__), "resources") + op.sep)
+
+
+def test_integration():
+
+    # Import data
+    data_img = nib.load(op.join(get_resources_path(), "data.nii.gz"))
+    mask_img = nib.load(op.join(get_resources_path(), "mask.nii.gz"))
+
+    breakpoint()
+
+    u, s, varex_norm, v = ma_pca(data_img, mask_img)
+
+    breakpoint()
