f190afa1dec747ed8547c1f2a65b3689be3cd53c
Eneko <enekouru@gmail.com>
2021-01-07 11:17:33 +0100

    Fix style

--- mapca/tests/conftest.py
+++ mapca/tests/conftest.py
@@ -5,6 +5,7 @@ def pytest_addoption(parser):
     parser.addoption('--skipintegration', action='store_true',
                      default=False, help='Skip integration tests.')
 
+
 @pytest.fixture
 def skip_integration(request):
     return request.config.getoption('--skipintegration')
