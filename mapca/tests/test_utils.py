412e89086d5077f2b595c4b98de77d1e2019b77a
Zaki A <zaki@live.ca>
2021-01-07 11:38:17 -0600

    Allow subsampling to accept ndarray

--- mapca/tests/test_utils.py
+++ mapca/tests/test_utils.py
@@ -81,13 +81,17 @@ def test_subsampling():
     Unit test for subsampling function
     """
     test_data = np.array([1])
-    with raises(ValueError) as errorinfo:
-        sub_data = _subsampling(test_data, 1)
-    assert "Unrecognized matrix dimension" in str(errorinfo.value)
-
+    sub_data = _subsampling(test_data, sub_depth=2)
+    assert sub_data.shape == (1,)
+    assert np.all(sub_data == test_data[::2])
+    test_data = np.random.rand(6, 9)
+    sub_data = _subsampling(test_data, sub_depth=3)
+    assert sub_data.shape == (2, 3)
+    assert np.all(sub_data == test_data[::3, ::3])
     test_data = np.random.rand(2, 3, 4)
     sub_data = _subsampling(test_data, sub_depth=2)
     assert sub_data.shape == (1, 2, 2)
+    assert np.all(sub_data == test_data[::2, ::2, ::2])
 
 
 def test_kurtn():
