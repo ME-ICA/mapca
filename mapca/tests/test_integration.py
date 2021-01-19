7ebd5cb53f4984c33c1a0de74f3c031188609365
Eneko <enekouru@gmail.com>
2021-01-19 21:32:45 +0100

    Add asserts to integration test

--- mapca/tests/test_integration.py
+++ mapca/tests/test_integration.py
@@ -5,6 +5,7 @@ Integration test for mapca.
 import os.path as op
 
 import nibabel as nib
+import numpy as np
 import pytest
 
 from mapca.mapca import ma_pca
@@ -35,3 +36,13 @@ def test_integration(skip_integration):
     mask_img = nib.load(op.join(get_resources_path(), "mask.nii.gz"))
 
     u, s, varex_norm, v = ma_pca(data_img, mask_img)
+
+    voxel_comp_weights = np.load(op.join(get_resources_path(), "voxel_comp_weights.npy"))
+    varex = np.load(op.join(get_resources_path(), "varex.npy"))
+    varex_norm_test = np.load(op.join(get_resources_path(), "varex_norm.npy"))
+    comp_ts = np.load(op.join(get_resources_path(), "comp_ts.npy"))
+
+    assert np.allclose(voxel_comp_weights, u)
+    assert np.allclose(varex, s)
+    assert np.allclose(varex_norm_test, varex_norm)
+    assert np.allclose(comp_ts, v)
