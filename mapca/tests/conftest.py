d68850fac5f8e5a2b09287e6e46f4e810e944741
Eneko <enekouru@gmail.com>
2021-01-23 09:59:45 +0100

    Remove resources folder and use osf repo

--- mapca/tests/conftest.py
+++ mapca/tests/conftest.py
@@ -1,3 +1,6 @@
+import os
+from urllib.request import urlretrieve
+
 import pytest
 
 
@@ -9,3 +12,73 @@ def pytest_addoption(parser):
 @pytest.fixture
 def skip_integration(request):
     return request.config.getoption('--skipintegration')
+
+
+def fetch_file(osf_id, path, filename):
+    """
+    Fetches file located on OSF and downloads to `path`/`filename`1
+
+    Parameters
+    ----------
+    osf_id : str
+        Unique OSF ID for file to be downloaded. Will be inserted into relevant
+        location in URL: https://osf.io/{osf_id}/download
+    path : str
+        Path to which `filename` should be downloaded. Ideally a temporary
+        directory
+    filename : str
+        Name of file to be downloaded (does not necessarily have to match name
+        of file on OSF)
+
+    Returns
+    -------
+    full_path : str
+        Full path to downloaded `filename`
+    """
+    url = 'https://osf.io/{}/download'.format(osf_id)
+    full_path = os.path.join(path, filename)
+    if not os.path.isfile(full_path):
+        urlretrieve(url, full_path)
+    return full_path
+
+
+@pytest.fixture(scope='session')
+def testpath(tmp_path_factory):
+    """ Test path that will be used to download all files """
+    return tmp_path_factory.getbasetemp()
+
+
+@pytest.fixture
+def test_img(testpath):
+    return fetch_file('jw43h', testpath,
+                      'data.nii.gz')
+
+
+@pytest.fixture
+def test_mask(testpath):
+    return fetch_file('9u2m5', testpath,
+                      'mask.nii.gz')
+
+
+@pytest.fixture
+def test_ts(testpath):
+    return fetch_file('gz2hb', testpath,
+                      'compt_ts.npy')
+
+
+@pytest.fixture
+def test_varex(testpath):
+    return fetch_file('7xj5k', testpath,
+                      'varex.npy')
+
+
+@pytest.fixture
+def test_varex_norm(testpath):
+    return fetch_file('jrd9c', testpath,
+                      'varex_norm.npy')
+
+
+@pytest.fixture
+def test_weights(testpath):
+    return fetch_file('t94m8', testpath,
+                      'voxel_comp_weights.npy')
--- mapca/tests/test_integration.py
+++ mapca/tests/test_integration.py
@@ -1,9 +1,6 @@
 """
 Integration test for mapca.
 """
-
-import os.path as op
-
 import nibabel as nib
 import numpy as np
 import pytest
@@ -11,37 +8,25 @@ import pytest
 from mapca.mapca import ma_pca
 
 
-def get_resources_path():
-    """Return the path to test resources.
-
-    Returns the path to test resources, terminated with separator.
-    Resources are kept outside package folder in "resources".
-    Based on function by Yaroslav Halchenko used in Neurosynth Python package.
-
-    Returns
-    -------
-    resource_path : str
-        Absolute path to resources folder.
-    """
-    return op.abspath(op.join(op.dirname(__file__), "resources") + op.sep)
-
-
-def test_integration(skip_integration):
+def test_integration(skip_integration, test_img, test_mask, test_ts, test_varex,
+                     test_varex_norm, test_weights):
 
     if skip_integration:
         pytest.skip('Skipping integration test')
 
     # Import data
-    data_img = nib.load(op.join(get_resources_path(), "data.nii.gz"))
-    mask_img = nib.load(op.join(get_resources_path(), "mask.nii.gz"))
+    data_img = nib.load(test_img)
+    mask_img = nib.load(test_mask)
+    # data_img = nib.load(op.join(get_resources_path(), "data.nii.gz"))
+    # mask_img = nib.load(op.join(get_resources_path(), "mask.nii.gz"))
     u, s, varex_norm, v = ma_pca(data_img, mask_img, normalize=True)
 
-    voxel_comp_weights = np.load(op.join(get_resources_path(), "voxel_comp_weights.npy"))
-    varex = np.load(op.join(get_resources_path(), "varex.npy"))
-    varex_norm_test = np.load(op.join(get_resources_path(), "varex_norm.npy"))
-    comp_ts = np.load(op.join(get_resources_path(), "comp_ts.npy"))
+    voxel_comp_weights = np.load(test_weights)
+    varex = np.load(test_varex)
+    v_norm = np.load(test_varex_norm)
+    comp_ts = np.load(test_ts)
 
     assert np.allclose(voxel_comp_weights, u)
     assert np.allclose(varex, s)
-    assert np.allclose(varex_norm_test, varex_norm)
+    assert np.allclose(v_norm, varex_norm)
     assert np.allclose(comp_ts, v)
