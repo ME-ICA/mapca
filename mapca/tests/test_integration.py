9249627b59b6cd56660a7ee55dd87a21dd7d5ec4
Eneko <enekouru@gmail.com>
2021-01-23 10:19:52 +0100

    Fix error

--- mapca/tests/test_integration.py
+++ mapca/tests/test_integration.py
@@ -13,9 +13,6 @@ from mapca.mapca import ma_pca
 def test_integration(test_img, test_mask, test_ts, test_varex,
                      test_varex_norm, test_weights):
 
-    if skip_integration:
-        pytest.skip('Skipping integration test')
-
     test_path, _ = split(test_img)
 
     # Import data
