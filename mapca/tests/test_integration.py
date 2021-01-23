6610340a839b06547a50c093b55b94fb51ef7461
Eneko <enekouru@gmail.com>
2021-01-23 10:18:02 +0100

    Remove skipintegration option

--- mapca/tests/test_integration.py
+++ mapca/tests/test_integration.py
@@ -10,7 +10,7 @@ from os.path import split
 from mapca.mapca import ma_pca
 
 
-def test_integration(skip_integration, test_img, test_mask, test_ts, test_varex,
+def test_integration(test_img, test_mask, test_ts, test_varex,
                      test_varex_norm, test_weights):
 
     if skip_integration:
