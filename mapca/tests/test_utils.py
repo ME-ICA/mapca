8df8c054744f9387ed1f6fe0a95914115859a3c2
Eneko <enekouru@gmail.com>
2021-01-26 19:40:28 +0100

    Add kurtosis test

--- mapca/tests/test_utils.py
+++ mapca/tests/test_utils.py
@@ -1,6 +1,8 @@
 """Unit tests for utils."""
 import numpy as np
 from pytest import raises
+from scipy.signal import detrend
+from scipy.stats import kurtosis
 
 from mapca.utils import (_autocorr, _check_order, _eigensp_adj, _icatb_svd,
                          _parzen_win, _subsampling, ent_rate_sp)
@@ -109,3 +111,23 @@ def test_eigensp_adj():
     test_result = np.array([0.13508894, 0.11653465, 0.06727316, 0.05211424, 0.0])
     lambd_adj = _eigensp_adj(test_eigen, n_effective, p=test_eigen.shape[0])
     assert np.allclose(lambd_adj, test_result)
+
+
+def test_kurtosis():
+    # Generate data
+    test_data = np.array([[-10, 2, 500, 0, -0.4], [-4, -200, -40, 0.1, 90]]).T
+
+    # Run scipy function
+    kurt_scipy = kurtosis(test_data, axis=0, fisher=True)
+    kurt_scipy[kurt_scipy < 0] = 0
+    kurt_scipy = np.expand_dims(kurt_scipy, 1)
+
+    # Calculate kurtosis like GIFT
+    kurt_gift = np.zeros((test_data.shape[1], 1))
+    for i in range(test_data.shape[1]):
+        data_norm = detrend(test_data[:, i], type='constant')
+        data_norm /= np.std(data_norm)
+        kurt_gift[i] = np.mean(data_norm**4) - 3
+    kurt_gift[kurt_gift < 0] = 0
+
+    assert np.allclose(kurt_gift, kurt_scipy)
